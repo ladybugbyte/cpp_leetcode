062e8b720e822fafcdae8a07421fec90a0365592
Danica Gajic <>
2021-02-10 06:31:45 +0100

    Added bazel workspace and BUILD files

--- src/two_sum_my_initial_solution.hpp
+++ src/two_sum_my_initial_solution.hpp
@@ -1,9 +1,11 @@
 // My first attempt to solve this problem - working
+#include <vector>
+using namespace std;
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+
         vector<int> result;
         
         for (int i = 0; i < nums.size() -1; i++) {
